6a512f3e88f416d919725e56dd94314e220c7498
Eudes EG. Gomes <Eudes@Patrimonio.Manaus>
2018-07-14 08:01:03 -0400

    primeiro commit

---
+++ LojaVirtual.Web/Data/LojaVirtualContext.cs
@@ -0,0 +1,24 @@
+﻿using LojaVirtual.Web.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace LojaVirtual.Web.Data
+{
+    public class LojaVirtualContext : DbContext
+    {
+        public LojaVirtualContext() : base("LojaVirtual")
+        {
+            //Configuration.ProxyCreationEnabled = false;
+            //Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ LojaVirtual.Web/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaVirtual.Web.Models
+{
+    
+    public partial class Produto
+    {
+        public int ID { get; set; }
+
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
